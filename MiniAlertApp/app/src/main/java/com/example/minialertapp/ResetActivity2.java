a8e0b2c9c37d9a9a94e8b8b0e0934e984e094d2d
Mahmuda073 <122888071+Mahmuda073@users.noreply.github.com>
2023-01-17 23:55:48 +0600

    Add files via upload

---
+++ MiniAlertApp/app/src/main/java/com/example/minialertapp/ResetActivity2.java
@@ -0,0 +1,14 @@
+package com.example.minialertapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResetActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset2);
+    }
+}
